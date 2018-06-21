da1c227955d36f014c871337dfd7f0e1192386c0
ChairMane <danielschris899@gmail.com>
2018-06-20 23:47:21 -0700

    Fixed return values

--- eBirdAPI.py
+++ eBirdAPI.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+from pprint import pprint
 
 config = json.load(open('config.json'))
 
@@ -9,7 +10,7 @@ class Bird_e:
     #Grabs recent observations within a local ID. regiona codes accepted:
     #subnational1, subnational2, eBird local ID or country code.
     def get_recent_observation(self, regional_code, maxResults=None, back=None):
-
+        sighting = {}
         eBird_key = config['ebird_token']
         final_url_obs = 'https://ebird.org/ws2.0/data/obs/' + regional_code + '/recent/?'
         if maxResults:
@@ -17,11 +18,20 @@ class Bird_e:
         if back:
             final_url_obs += 'back=' + back + '&'
         final_url_obs += 'key=' + eBird_key
-        print(final_url_obs)
+
         obs = requests.get(final_url_obs)
         observations = obs.json()
-        for dict in observations:
-            print(dict['comName'] + ' was found in ' + dict['locName'])
+
+        for dictionary in observations:
+            sighting[dictionary['comName']] = dictionary['locName']
+
+        results = ''
+        i = 1
+        for key in sighting:
+            results += str(i) + ') ' + key + ' seen here: \n' + '     ' + sighting[key] + '\n\n'
+            i += 1
+
+        return results
 
     #Grabs recent notabel observations within a local ID. regiona codes accepted:
     #subnational1, subnational2, eBird local ID or country code.
@@ -94,18 +104,3 @@ class Bird_e:
         observations = obs.json()
         for dict in observations:
             print(dict['comName'] + ' was found within ' + dist + 'km of your coordinates at ' + dict['locName'] + '.')
-
-bird = Bird_e()
-
-#regional_code = input("Enter regional code here: ")
-speciescode = input("Enter species code: ")
-lat = input("Enter latitude coordinate: ")
-lng = input("Enter longitude coordinate: ")
-dist = input("Enter radius from coordinates: ")
-maxresults = input("Enter max results: ")
-back = input("Enter how far back you want to go: ")
-bird.get_recent_species_nearby_observations(speciescode, lat, lng, dist, maxresults, back)
-#bird.get_recent_species_observation(regional_code, speciescode, maxresults, back)
-#bird.get_recent_observation(regional_code, maxresults, back)
-#print("Below is recent notable observations: ")
-#bird.get_recent_notable_observation(regional_code, maxresults, back)
